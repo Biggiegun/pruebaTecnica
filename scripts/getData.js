b3e5ca93e967bf5a332a5b641aa08dd2822d5320
David Herrera <biggiegun@gmail.com>
2021-10-08 11:35:56 -0500

    Consulta de imágenes url asignada

---
+++ scripts/getData.js
@@ -0,0 +1,32 @@
+let myBtn = document.getElementById('btnImagenes');
+
+
+const getPhotos = async (source) =>{
+    let mostrarFotos = document.querySelector(".grid-imagenes")
+    mostrarFotos.innerHTML = '';
+    const resp = await fetch(source);
+    console.log(resp);
+    const data = await resp.json();
+    console.log(data);
+    data.forEach(item=>{
+        const {url, title} = item;
+        mostrarFotos.innerHTML +=`
+        <div class="col mascotas">
+        <a href="#" class="enlace-detalle-mascota">
+            <div class="card bg-dark text-white gradiente">                
+            <img src="${url}" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                        <p class="card-text body2Regular">${title}</p>
+                </div>
+            </div>
+        </a>
+    </div>
+        `
+    })
+}
+
+/*////////////////////////////////////*/
+
+myBtn.addEventListener('click', ()=>{getPhotos('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=50')})
+
+//myBtn.addEventListener('click', ()=> alert('hola'))
